d3f52bc73d8b650be3b1e0d1aa4dc4ace42da023
MFR <102693974+mfr-fr@users.noreply.github.com>
2022-11-08 21:28:13 +0100

    Update dox.js

--- dox.js
+++ dox.js
@@ -1,25 +1,5 @@
-//----------------------------------------------------------------------------------------------------------------------------------------
-//
-//BBBBBBBBBBBBBBBBB    YYYYYYY       YYYYYYY                 MMMMMMMM               MMMMMMMM FFFFFFFFFFFFFFFFFFFFFF RRRRRRRRRRRRRRRRR   
-//B::::::::::::::::B   Y:::::Y       Y:::::Y                 M:::::::M             M:::::::M F::::::::::::::::::::F R::::::::::::::::R  
-//B::::::BBBBBB:::::B  Y:::::Y       Y:::::Y                 M::::::::M           M::::::::M F::::::::::::::::::::F R::::::RRRRRR:::::R 
-//BB:::::B     B:::::B Y::::::Y     Y::::::Y                 M:::::::::M         M:::::::::M FF::::::FFFFFFFFF::::F RR:::::R     R:::::R
-//  B::::B     B:::::B YYY:::::Y   Y:::::YYY                 M::::::::::M       M::::::::::M  F:::::F       FFFFFF  R::::R     R:::::R
-//  B::::B     B:::::B    Y:::::Y Y:::::Y                    M:::::::::::M     M:::::::::::M  F:::::F                R::::R     R:::::R
-//  B::::BBBBBB:::::B      Y:::::Y:::::Y                     M:::::::M::::M   M::::M:::::::M  F::::::FFFFFFFFFF      R::::RRRRRR:::::R 
-//  B:::::::::::::BB        Y:::::::::Y      --------------- M::::::M M::::M M::::M M::::::M  F:::::::::::::::F      R:::::::::::::RR  
-// B::::BBBBBB:::::B        Y:::::::Y       -:::::::::::::- M::::::M  M::::M::::M  M::::::M  F:::::::::::::::F      R::::RRRRRR:::::R 
-//  B::::B     B:::::B        Y:::::Y        --------------- M::::::M   M:::::::M   M::::::M  F::::::FFFFFFFFFF      R::::R     R:::::R
-//  B::::B     B:::::B        Y:::::Y                        M::::::M    M:::::M    M::::::M  F:::::F                R::::R     R:::::R
-//  B::::B     B:::::B        Y:::::Y                        M::::::M     MMMMM     M::::::M  F:::::F                R::::R     R:::::R
-//BB:::::BBBBBB::::::B        Y:::::Y                        M::::::M               M::::::M FF:::::::FF            RR:::::R     R:::::R
-//B:::::::::::::::::B      YYYY:::::YYYY                     M::::::M               M::::::M F::::::::FF            R::::::R     R:::::R
-//B::::::::::::::::B       Y:::::::::::Y                     M::::::M               M::::::M F::::::::FF            R::::::R     R:::::R
-//BBBBBBBBBBBBBBBBB        YYYYYYYYYYYYY                     MMMMMMMM               MMMMMMMM FFFFFFFFFFF            RRRRRRRR     RRRRRRR
-//                                                                                             
-//--------------------------------------------------------------------------------------------------------------------------------------->
-
-function search_animal() {
+// dev-by-mfr                                                                                  
+function search_dox() {
   let input = document.getElementById('searchbar').value
   input=input.toLowerCase();
   let x = document.getElementsByClassName('bar');
